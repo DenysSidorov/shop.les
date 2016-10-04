efc6070f3cff10bf94a5f4a27a1cf7537153ea74
DenysSidorov <000scorpions000@gmail.com>
2016-10-04 14:00:42 +0300

    bag fix, add login and logout, add editting user data(pass and mail), add views and check user data

---
+++ test/html.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Denis
+ * Date: 03.10.2016
+ * Time: 23:26
+ */
+
+
+?>
+<form method="get" action="../models/Temp.php" name="myForm">
+ <select name="namef" onChange="document.forms.myForm.submit ()">
+    <option value='8'  selected>1</option>
+    <option value='10' >2</option>
+    <option value='15' >3</option>
+
+ </select>
+</form>
