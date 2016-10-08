9fad6c781e0258009dbeb2e8427494f694b709a2
DenysSidorov <000scorpions000@gmail.com>
2016-10-08 21:39:30 +0300

    without PHP7

--- config/db_params.php
+++ config/db_params.php
@@ -2,8 +2,13 @@
 
 return
     array(
-    'host' => 'localhost',
-    'dbname' => 'phpshop',
-    'user' => 'root',
-    'password' => '',
+    'host' => 'mysql.zzz.com.ua',
+    'dbname' => 'denis2222',
+    'user' => 'denis2222',
+    'password' => 'PASSword',
+//    array(
+//    'host' => 'localhost',
+//    'dbname' => 'phpshop',
+//    'user' => 'root',
+//    'password' => '',
 );
